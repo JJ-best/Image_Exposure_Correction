efc0c917626d4c6ae9454345a5a4de6c0ecbee85
JJ-best <jesse.mojo@gmail.com>
2025-12-05 21:35:23 +0800

    2025.12.5 update the todo list

--- py/py_overlap_partition/main.py
+++ py/py_overlap_partition/main.py
@@ -44,20 +44,27 @@ if __name__ == "__main__":
             
             # enhanced1_patch(32x32x3)
             enhanced1_patch = run_lime(out_dir=out_dir, img_in=patch_img, k0=30, gamma= 0.7)
-            # enhanced1_img
+            # enhanced1_img(24x24x3)
             enhanced1_img[i*valid_size:(i+1)*valid_size, j*valid_size:(j+1)*valid_size, 0:3] = enhanced1_patch[border:border+valid_size, border:border+valid_size, 0:3]
             
             arr_inv = 1 - patch_img
 
             # enhanced2_patch(32x32x3)
             enhanced2_patch = run_lime(out_dir=out_dir, img_in=arr_inv, k0=30, gamma= 0.7)
+            # enhanced2_img(24x24x3)
             enhanced2_img[i*valid_size:(i+1)*valid_size, j*valid_size:(j+1)*valid_size, 0:3] = enhanced2_patch[border:border+valid_size, border:border+valid_size, 0:3]
             
             enhanced2_patch_inv = 1- enhanced2_patch
             enhanced2_inv[i*valid_size:(i+1)*valid_size, j*valid_size:(j+1)*valid_size, 0:3] = enhanced2_patch_inv[border:border+valid_size, border:border+valid_size, 0:3]
             
-            # function input enhanced1_patch, enhanced2_patch, arr
-            # output 32x32x3 matrix
+            '''
+            todo: 
+            function input enhanced1_patch, enhanced2_patch_inv, arr(32x32x3 matrix)
+            discard the outter boundary, only use the inner 24x24x3 C, S, E matrix
+            note: you may use 25x25x3 matrix to calculate the 24x24x3 C matrix
+            output one 24x24x3 matrix(I will combine these matrix block)
+            '''
+            
             
 
     # --------------------------------------------------------- #
