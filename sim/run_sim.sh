ef512364478993ef7d8fdc48d89df3a0a3131568
nina1032024 <yucenc4@gmail.com>
2025-12-08 10:54:59 +0800

    update sim script

--- sim/run_sim.sh
+++ sim/run_sim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # PAT: imgs_lime1 -> 1;  imgs_lime1 -> 2
 # PATCH_I, PATCH_J: patch_PATCH_I_PATCH_J_over/under.bmp
+# INIT_EN: Initialize mode -> 1; hardware mode -> 0
 
 # Layer0: input_image #
 # vcs -R +v2k -full64 -f sim.f -debug_acc+all -l vcs_layer0.log \
