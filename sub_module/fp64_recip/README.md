083a7b5113004cbce2761e8ef4ba783123683ab5
JJ-best <jesse.mojo@gmail.com>
2025-12-10 11:24:01 +0800

    2025.12.10 submodule fp64 recip file

---
+++ sub_module/fp64_recip/README.md
@@ -0,0 +1,1 @@
+# Floating Point 64 Reciprocal
---
+++ sub_module/fp64_recip/hdl/fp64_recip.v
@@ -0,0 +1,13 @@
+module fp64_recip #(
+    parameter pFP_WIDTH = 64
+)(
+    input clk,
+    input srst_n,
+    input [(pFP_WIDTH-1):0] in_A    // write input data in tb
+    input in_valid,                 // pull input valid in tb
+    output [(pFP_WIDTH-1):0] in_B,
+    output output_valid
+);
+// please make sure that the period can be faster than 3ns
+
+endmodule //fp64_recip
