e1f401d5cd0220f7742d649fca66af684500c740
JJ-best <jesse.mojo@gmail.com>
2025-12-21 22:37:37 +0800

    2025.12.21 upload sim.f

--- sim/sim.f
+++ sim/sim.f
@@ -7,43 +7,51 @@ sram_model/sram_256x8b.v
 sram_model/sram_512x8b.v
 sram_model/sram_256x16b.v
 sram_model/sram_64x16b_16bank.v
-
-// module 
+sram_model/twiddle_rom.v
+// top module 
 ../hdl/IEC_top.v
+// fft
+../submodule/fft/hdl/fft.v
+../submodule/fft/hdl/bpe.v
+// int2fp
+../submodule/int2fp/hdl/int2fp.v
+// fp64_recip
+../submodule/fp64_recip/hdl/fp64_recip.v
+../submodule/fp64_recip/hdl/LUT7.v
+// mul
+../submodule/mul/hdl/mul.v
+../submodule/mul/hdl/mul16_array_ntt.v
+../submodule/mul/hdl/mul16_array.v
+../submodule/mul/hdl/mul_64.v
+../submodule/mul/hdl/mul_53.v
+../submodule/mul/hdl/mul_16.v
+../submodule/mul/hdl/butterfly.v
+../submodule/mul/hdl/fp_add.v
+../submodule/mul/hdl/fmul_exp.v
+../submodule/mul/hdl/fmul_rounder.v
+../submodule/mul/hdl/wallace_131.v
+../submodule/mul/hdl/LOD_128.v
+../submodule/mul/hdl/LOD_64.v
+../submodule/mul/hdl/CLA33.v
+../submodule/mul/hdl/CLA17.v
+../submodule/mul/hdl/CLA_8.v
+../submodule/mul/hdl/add_107.v
+../submodule/mul/hdl/add_58.v
+../submodule/mul/hdl/add_54.v
+../submodule/mul/hdl/add_53_overflow.v
+../submodule/mul/hdl/add_53.v
+../submodule/mul/hdl/add_13_overflow.v
+../submodule/mul/hdl/add_13.v
+../submodule/mul/hdl/add_12_overflow.v
+../submodule/mul/hdl/add_11_overflow.v
+../submodule/mul/hdl/sub_107.v
+../submodule/mul/hdl/sub_58.v
+../submodule/mul/hdl/sub_13.v
+../submodule/mul/hdl/sub_12.v
+../submodule/mul/hdl/mont_add.v
+../submodule/mul/hdl/mont_sub.v
+../submodule/mul/hdl/FA.v
+../submodule/mul/hdl/HA.v
 
-// sub modules
-../sub_module/int2fp/hdl/int2fp.v
-../sub_module/mul/hdl/mul.v
-../sub_module/mul/hdl/mul16_array.v
-../sub_module/mul/hdl/mul16_array_ntt.v
-../sub_module/mul/hdl/wallace_131.v
-../sub_module/mul/hdl/fmul_exp.v
-../sub_module/mul/hdl/fmul_rounder.v
-../sub_module/mul/hdl/fp_add.v
-../sub_module/mul/hdl/add_107.v
-../sub_module/mul/hdl/add_11_overflow.v
-../sub_module/mul/hdl/add_12_overflow.v
-../sub_module/mul/hdl/add_13_overflow.v
-../sub_module/mul/hdl/add_13.v
-../sub_module/mul/hdl/add_53_overflow.v
-../sub_module/mul/hdl/add_53.v
-../sub_module/mul/hdl/add_54.v
-../sub_module/mul/hdl/add_58.v
-../sub_module/mul/hdl/butterfly.v
-../sub_module/mul/hdl/CLA17.v
-../sub_module/mul/hdl/CLA33.v
-../sub_module/mul/hdl/CLA_8.v
-../sub_module/mul/hdl/FA.v
-../sub_module/mul/hdl/HA.v
-../sub_module/mul/hdl/LOD_128.v
-../sub_module/mul/hdl/LOD_64.v
-../sub_module/mul/hdl/mont_add.v
-../sub_module/mul/hdl/mont_sub.v
-../sub_module/mul/hdl/mul_16.v
-../sub_module/mul/hdl/mul_53.v
-../sub_module/mul/hdl/mul_64.v
-../sub_module/mul/hdl/sub_107.v
-../sub_module/mul/hdl/sub_12.v
-../sub_module/mul/hdl/sub_13.v
-../sub_module/mul/hdl/sub_58.v
+
 
